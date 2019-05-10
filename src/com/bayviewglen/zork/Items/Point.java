fd9505f1cdde6418a74bbf5a9fc19083522c6b5a
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-10 14:03:34 -0400

    Fixed item id

--- src/com/bayviewglen/zork/Items/Point.java
+++ src/com/bayviewglen/zork/Items/Point.java
@@ -2,7 +2,7 @@ package com.bayviewglen.zork.Items;
 public class Point extends Item{
 	
 	public Point() {
-		super(6, "Point", "A part of the battering ram that is attatched to the cylinder and smashes the door", false, 100, 1);
+		super(7, "Point", "A part of the battering ram that is attatched to the cylinder and smashes the door", false, 100, 1);
 	}
 
 }
