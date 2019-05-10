b103875cece9b11f2b354c09dc28dfc342a7d43b
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-10 15:37:36 -0400

    Two more items and their classes added.

---
+++ src/com/bayviewglen/zork/Items/Garlic.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork.Items;
+public class Garlic extends Item{
+	
+	public Garlic() {
+		super(8, "Garlic", "An uncut piece of garlic with a strong aura", true, 20, 1);
+	}
+
+}
