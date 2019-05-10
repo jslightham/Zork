029d8d7cf5a38624cd089a914b6955b0eee58dd7
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-10 15:39:51 -0400

    Fixed spelling error.

--- src/com/bayviewglen/zork/Items/Robes.java
+++ src/com/bayviewglen/zork/Items/Robes.java
@@ -2,7 +2,7 @@ package com.bayviewglen.zork.Items;
 public class Robes extends Item{
 	
 	public Robes(){
-		super(9, "Robes", "Worn out robes that belonged to Henry Pelatt", false, 50, 1);
+		super(9, "Robes", "Worn out robes that belonged to Henry Pellatt", false, 50, 1);
 	}
 
 }
