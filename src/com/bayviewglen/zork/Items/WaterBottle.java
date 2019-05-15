13837003c2cde9043b8288c29baa0be46a821baf
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-14 22:29:07 -0400

    New items and classes added

---
+++ src/com/bayviewglen/zork/Items/WaterBottle.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork.Items;
+public class WaterBottle extends Item{
+	
+	public WaterBottle() {
+		super(10, "Water Bottle", "A plastic water bottle half filled with filthy water", true, 10, 1);
+	}
+
+}
