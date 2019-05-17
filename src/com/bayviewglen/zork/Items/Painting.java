4506c1e17f8f442c5602df217b65f1e6294c12d6
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-16 20:47:49 -0400

    Items and classes added

---
+++ src/com/bayviewglen/zork/Items/Painting.java
@@ -0,0 +1,7 @@
+package com.bayviewglen.zork.Items;
+public class Painting extends Item{
+	
+	public Painting(){
+		super(14, "Painting", "An antique painting made from vibrant colours with an artists signature in the bottom right corner", false, 50, 1);
+	}
+}
