fa0cd81a81e402a610341a51606abf257329480b
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-23 13:18:25 -0400

    Updated enemy class

--- src/com/bayviewglen/zork/Entities/Enemy.java
+++ src/com/bayviewglen/zork/Entities/Enemy.java
@@ -1,5 +1,16 @@
 package com.bayviewglen.zork.Entities;
 
 public class Enemy {
+	int damageGiven; 
+	
+	public Enemy(){
+		super(); 
+		damageGiven = 10; 
+	}
+	
+	public Enemy(int damageGiven){
+		super(); 
+		this.damageGiven = damageGiven; 
+	}
 	
 }
