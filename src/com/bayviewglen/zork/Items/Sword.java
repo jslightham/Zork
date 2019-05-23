d1337bdea7c5ecba3faa0b829d42e26da6ba2008
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-23 13:46:12 -0400

    Sword item added

---
+++ src/com/bayviewglen/zork/Items/Sword.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork.Items;
+public class Sword extends Item{
+	
+	public Sword(){
+		super(33, "Sword", "A steel, double-edged sword which seems to have been sharpened recently", 20, 50);
+	}
+
+}
