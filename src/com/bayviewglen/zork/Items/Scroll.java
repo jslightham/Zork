36fe007dd8218f9ebd3f975dd3dc850a23c4babe
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-27 15:12:23 -0400

    updated some things

---
+++ src/com/bayviewglen/zork/Items/Scroll.java
@@ -0,0 +1,11 @@
+package com.bayviewglen.zork.Items;
+
+public class Scroll {
+	private String message; 
+	public Scroll(String message) {
+		this.message = message; 
+	}
+	public String getMessage() {
+		return message;
+	}
+}
