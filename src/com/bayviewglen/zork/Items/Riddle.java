9d073ef6425dd52d1061584c384f1a2fd760e5f9
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-27 22:54:53 -0400

    finish commit "made riddle class"

--- src/com/bayviewglen/zork/Items/Riddle.java
+++ src/com/bayviewglen/zork/Items/Riddle.java
@@ -1,5 +1,12 @@
 package com.bayviewglen.zork.Items;
 
 public class Riddle {
+	String question; 
+	String answer; 
+	
+	public Riddle(String question, String answer) {
+		this.question = question; 
+		this.answer = answer; 
+	}
 
 }
