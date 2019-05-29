81c8b92f1eee5803e240176610a5f3372b66be75
elacey <elacey@elaceysp4.bayviewglen.ca>
2019-05-29 13:24:48 -0400

    New items and classes added

---
+++ src/com/bayviewglen/zork/Items/Lightbulb.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.zork.Items;
+
+public class Lightbulb extends Item{
+	
+	public Lightbulb() {
+		super(35, "Lightbulb", "An incandescent light bulb rolls around on the floor", false, 20, 5);
+	}
+
+}
