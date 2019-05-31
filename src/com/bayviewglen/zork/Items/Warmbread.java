ad08e5482a4dc786d0707f7dafe4892f7387275a
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-31 17:34:05 -0400

    commit

--- src/com/bayviewglen/zork/Items/Warmbread.java
+++
@@ -1,9 +0,0 @@
-package com.bayviewglen.zork.Items;
-
-public class Warmbread extends Item{
-	
-	public Warmbread() {
-		super(25, "Warm Bread", "A freshly baked loaf of bread sits ominously on the counter", true, 20, 10);
-	}
-
-}
--- src/com/bayviewglen/zork/Items/Waterbottle.java
+++
@@ -1,8 +0,0 @@
-package com.bayviewglen.zork.Items;
-public class Waterbottle extends Item{
-	
-	public Waterbottle() {
-		super(10, "Water Bottle", "A plastic water bottle half filled with filthy water", true, 10, 1);
-	}
-
-}
