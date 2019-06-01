671f6eacb0f050dea6fab3ab6a415172191bdbbb
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-31 20:59:14 -0400

    Made warmbread class

---
+++ src/com/bayviewglen/zork/Items/Warmbread.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.zork.Items;
+
+public class Warmbread extends Item{
+
+	public Warmbread() {
+		super(45, "Warm Bread", "A freshly baked bun, with a crackly golden crust", true, 25, 1); 
+	}
+
+}
